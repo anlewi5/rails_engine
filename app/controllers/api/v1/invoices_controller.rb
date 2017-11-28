fef86cdcfb8e180c8669d41810000b27318f8d3c
Dione Wilson <dionew1@gmail.com>
2017-11-27 21:46:17 -0700

    Created invoice_request_spec

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,10 +1,9 @@
-class Api::V1::InvoiceController < ApplicationController
-
+class Api::V1::InvoicesController < ApplicationController
   def index
     render json: Invoice.all
   end
 
   def show
-    render json: Invoice.find(param[:id])
+    render json: Invoice.find(params[:id])
   end
 end

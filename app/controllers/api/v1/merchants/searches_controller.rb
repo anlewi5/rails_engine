a37850fa97dbb946d8be0cfb6c669836c80d1056
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-27 20:41:14 -0700

    make merchant find tests pass

---
+++ app/controllers/api/v1/merchants/searches_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Merchants::SearchesController < ApplicationController
+  def index
+    render json: Merchant.all
+  end
+
+  def show
+    if params["id"]
+      render json: Merchant.find(params["id"])
+    elsif params["name"]
+      render json: Merchant.find_by(name: params["name"])
+    elsif params["created_at"]
+      render json: Merchant.find_by(created_at: params["created_at"])
+    elsif params["updated_at"]
+      render json: Merchant.find_by(updated_at: params["updated_at"])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get '/find', to: 'searches#index'
+        get '/find', to: 'searches#show'
       end
     end
   end

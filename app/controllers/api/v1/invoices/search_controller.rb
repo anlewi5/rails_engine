ceb957f394b18e1d1a38b258574e3ec0a08d4309
Dione Wilson <dionew1@gmail.com>
2017-11-28 15:21:15 -0700

    Add random for invoice

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::Invoices::SearchController < ApplicationController
   def index
     render json: Invoice.search_all(params)
   end
+
   def show
     render json: Invoice.search(params)
   end
+
 end

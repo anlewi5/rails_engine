8d5196e5f559894b7fd9f5b732cc1db853a4f166
Dione Wilson <dionew1@gmail.com>
2017-11-28 14:00:39 -0700

    Setup Search for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
       resources :invoices,  only: [:index, :show]
+      namespace :invoices do
+        get '/find_all', to: 'search#index'
+        get '/find',     to: 'search#show'
+      end
     end
   end
 end

e4f954e5eaa756c9682ff3943be2063a93299d6d
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-27 18:55:02 -0700

    make merchants_request_spec pass tests

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "lists all merchants" do
+    create_list(:merchant, 3)
+
+    get "/api/v1/merchants"
+
+    merchants = JSON.parse(response.body)
+    merchant = merchants.first
+
+    expect(response).to be_success
+    expect(merchants.count).to eq(3)
+    expect(merchant).to have_key("id")
+    expect(merchant).to have_key("name")
+    expect(merchant).to have_key("updated_at")
+    expect(merchant).to have_key("created_at")
+  end
+
+  it "can get single merchant by id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
+  end
+
+end

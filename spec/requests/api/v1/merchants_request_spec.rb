15a89b138b1b83e975ae1a77447058f2029b3e3b
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-27 18:40:13 -0700

    make merchants_request_spec pass

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,13 +7,13 @@ describe "Merchants API" do
     get '/api/v1/merchants'
 
     merchants = JSON.parse(response.body)
-    merchant = merchant.first
+    merchant = merchants.first
 
     expect(response).to be_success
     expect(merchants.count).to eq(3)
-    expect(merchant).to have(:id)
-    expect(merchant).to have(:name)
-    expect(merchant).to have(:updated_at)
-    expect(merchant).to have(:created_at)
+    expect(merchant).to have_key("id")
+    expect(merchant).to have_key("name")
+    expect(merchant).to have_key("updated_at")
+    expect(merchant).to have_key("created_at")
   end
 end

ad153d268145e8321243575050e5dd1e8364af95
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-27 20:04:24 -0700

    add searches controller for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      namespace :invoices do
-        get '/find_all', to: 'search#index'
-        get '/find',     to: 'search#show'
-        get '/random',   to: 'search#show'
+      namespace :merchants do
+        get '/find', to: 'searches#index'
       end
-      resources :merchants, only: [:index, :show]
-      resources :invoices,  only: [:index, :show]
-      resources :items,     only: [:index, :show]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show]
+    end
+  end
+
 end

bfd7bf2417904ed15c0c6ededd5327b30bc62cf4
Dione Wilson <dionew1@gmail.com>
2017-11-28 17:41:42 -0700

    Add routes and controller actions to items controller

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,39 +2,21 @@ require 'rails_helper'
 
 describe "Items API" do
   it "can list all items" do
-    customer = create(:customer)
-    merchant = create(:merchant)
-    create_list(:invoice, 3, customer_id: customer.id, merchant_id: merchant.id)
+    merchant = create!(:merchant)
+    create_list(:items, 4, merchant_id: merchant.id)
 
-    get "/api/v1/items"
+    get "api/v1/items"
 
-    invoices = JSON.parse(response.body)
-    invoice = invoices.first
-
-    expect(response).to be_success
-    expect(invoices.count).to eq 3
-    expect(invoice).to have_key("id")
-    expect(invoice).to have_key("status")
-    expect(invoice).to have_key("status")
-    expect(invoice).not_to have_key("created_at")
-    expect(invoice).not_to have_key("updated_at")
-    expect(invoice).to have_key("customer_id")
-    expect(invoice).to have_key("merchant_id")
-  end
-
-  it "can show single invoice" do
-    customer = create(:customer)
-    merchant = create(:merchant)
-    invoice = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
-
-    get "/api/v1/invoices/#{invoice.id}"
-
-    invoice_response = JSON.parse(response.body)
+    items = JSON.parse(response.body)
+    item = items.first
 
     expect(response).to be_success
-    expect(invoice_response["id"]).to eq(invoice.id)
-    expect(invoice_response["status"]).to eq(invoice.status)
-    expect(invoice_response["customer_id"]).to eq(customer.id)
-    expect(invoice_response["merchant_id"]).to eq(merchant.id)
+    epect(items.count).to eq 4
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("unit_price")
+    # expect(invoice).not_to have_key("created_at")
+    # expect(invoice).not_to have_key("updated_at")
   end
 end

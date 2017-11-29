57c11dcfeab945cce33d6e61a93d25cef3a1c9fd
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-28 19:40:27 -0700

    magic refactoring

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -81,63 +81,51 @@ describe "Merchants API" do
     end
 
     describe "find_all?" do
-      before(:each) do
-        @merchant1 = create(:merchant,
-                            id: 1,
-                            name: "MerchantName1",
-                            created_at: "2014-03-06T16:54:31",
-                            updated_at: "2015-03-06T16:54:31"
-                           )
-        @merchant2 = create(:merchant,
-                            id: 2,
-                            name: "MerchantName2",
-                            created_at: "2013-03-06T16:54:31",
-                            updated_at: "2014-03-06T16:54:31"
-                           )
-        @merchant3 = create(:merchant,
-                            id: 3,
-                            name: "MerchantName2",
-                            created_at: "2013-03-06T16:54:31",
-                            updated_at: "2014-03-06T16:54:31"
-                           )
+      subject { get "/api/v1/merchants/find_all?#{params}" }
+      let(:merchant_response) { JSON.parse(response.body) }
+
+      before do
+        create :merchant, id: 1,
+                          name: "DifferentMerchant",
+                          created_at: "2014-03-06T16:54:31",
+                          updated_at: "2015-03-06T16:54:31"
+        create :merchant, id: 2,
+                          name: "SameMerchant",
+                          created_at: "2013-03-06T16:54:31",
+                          updated_at: "2014-03-06T16:54:31"
+        create :merchant, id: 3,
+                          name: "SameMerchant",
+                          created_at: "2013-03-06T16:54:31",
+                          updated_at: "2014-03-06T16:54:31"
       end
 
-      shared_examples_for 'a response that finds merchants 2 and 3' do
+      shared_examples_for 'a response that finds merchants' do |*merchant_ids|
         it "finds the correct merchants" do
-          merchant_response = JSON.parse(response.body)
-
+          subject
           expect(response).to be_success
-          expect(merchant_response).to be_an(Array)
-          expect(merchant_response.count).to eq(2)
-          expect(merchant_response.map { |result| result['id'] })
-            .to contain_exactly 2, 3
+          expect(merchant_response).to be_an Array
+          expect(merchant_response.map { |result| result['id'] }).to contain_exactly(*merchant_ids)
         end
       end
 
-      it "can find merchants based on id" do
-        get "/api/v1/merchants/find_all?id=#{@merchant2.id}"
-
-        merchant_response = JSON.parse(response.body)
-
-        expect(response).to be_success
-        expect(merchant_response).to be_an(Array)
-        expect(merchant_response.count).to eq(1)
-        expect(merchant_response.first["id"]).to eq(@merchant2.id)
+      context 'based on id' do
+        let(:params) { "id=2" }
+        it_behaves_like 'a response that finds merchants', 2
       end
 
-      context "finding merchants based on name" do
-        before { get "/api/v1/merchants/find_all?name=#{@merchant2.name}" }
-        it_behaves_like 'a response that finds merchants 2 and 3'
+      context "based on name" do
+        let(:params) { "name=SameMerchant" }
+        it_behaves_like 'a response that finds merchants', 2, 3
       end
 
-      context "finding merchants based on created_at" do
-        before { get "/api/v1/merchants/find_all?created_at=#{@merchant2.created_at}" }
-        it_behaves_like 'a response that finds merchants 2 and 3'
+      context "based on created_at" do
+        let(:params) { "created_at=2013-03-06T16:54:31" }
+        it_behaves_like 'a response that finds merchants', 2, 3
       end
 
-      context "finding merchants based on updated_at" do
-        before { get "/api/v1/merchants/find_all?updated_at=#{@merchant2.updated_at}" }
-        it_behaves_like 'a response that finds merchants 2 and 3'
+      context "based on updated_at" do
+        let(:params) { "updated_at=2014-03-06T16:54:31" }
+        it_behaves_like 'a response that finds merchants', 2, 3
       end
     end
   end

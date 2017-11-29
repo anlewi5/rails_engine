cf26740bd90631780518016ebd4b3faaa56684e6
Dione Wilson <dionew1@gmail.com>
2017-11-28 17:28:35 -0700

    Create Invoice Items facotry with faker

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :invoice_item do
-    
+    sequence :quantity do
+      Faker::Number.digit
+    end
+    sequence :unit_price  do
+      Faker::Commerce.price
+    end
   end
 end

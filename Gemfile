19407a66698074591887dd33b5607c89c6662189
Dione Wilson <dionew1@gmail.com>
2017-11-28 17:24:46 -0700

    Create Items Factory

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development, :test do
   gem 'factory_bot_rails', '~> 4.0'
   #Shoulda
   gem 'shoulda-matchers', '~> 3.1'
+  gem 'faker'
   gem 'database_cleaner'
 end
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    sequence :name  do
+      Faker::Coffee.blend_name
+    end
+    sequence :description  do
+      Faker::Coffee.notes
+    end
+    sequence :unit_price do
+      Faker::Commerce.price
+    end
   end
 end
